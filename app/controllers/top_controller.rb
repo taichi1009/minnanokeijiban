e9253277b5c5e77b130428d13bc3210337e0a666
Taichi Koido <taichi1009@gmail.com>
2016-12-02 21:49:51 +0900

    second commit

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -3,7 +3,7 @@ class TopController < ApplicationController
   def index
     @question = Question.new
     # @questions = current_user.group.questions
-    feed_contents = current_user.group.feed_contents.includes(:content)
-    @feed_contents = feed_contents.map(&:content)
+    @feed_contents = current_user.group.feed_contents.includes(:content).page(params[:page]).per(5)
+    @feed_contents_resource = @feed_contents.map(&:content)
   end
 end
